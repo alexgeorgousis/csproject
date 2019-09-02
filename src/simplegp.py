a0ee7dba26c4494ccf8f093069651c1d4d53f396
Alexandros Georgousis <alexgeo2395@gmail.com>
2019-09-03 01:04:52 +0300

    Simple GP: implemented expression evaluation, including function application

--- src/simplegp.py
+++ src/simplegp.py
@@ -12,6 +12,7 @@ tset = ['x', '-2', '-1', '0', '1', '2']
 
 n = 4
 depth = 2
+solution = []  # x^2 + x + 1
 
 def init(n):
 	population = []
@@ -21,11 +22,54 @@ def init(n):
 	
 	return population
 
-def fitness(p):
+def fitness(p, solution):
 	"""
-	Computes the fitness of the program p.
+	Computes the fitness of the program p against the solution.
 	"""
+
+def eval(exp, x):
+	"""
+	Evaluates an expression, using the given the value for x.
+	"""
+
+	value = 0
+
+	# If the expression is a function
+	if isinstance(exp, list):
+		func = exp[0]
+
+		# TODO: don't assume function arity (num of arguments)
+		arg1 = eval(exp[1], x)
+		arg2 = eval(exp[2], x)
+
+		value = apply(func, arg1, arg2)
 	
+	# If the expression is a terminal
+	else:
+		if exp == 'x':                   # variable
+			value = x
+		elif isinstance(int(exp), int):  # constant
+			value = int(exp)
+
+	return value
+	
+# TODO: don't assume function arity
+def apply(func, arg1, arg2):
+	"""
+	Applies function func to arguments.
+	"""
+
+	result = 0
+
+	if func in fset:
+		if func == '+':
+			result = arg1 + arg2
+		elif func == '-':
+			result = arg1 - arg2
+		elif func == '*':
+			result = arg1 * arg2
+
+	return result
 
 def gen_rnd_exp(fset, tset, max_depth):
 	exp = ""
@@ -39,7 +83,7 @@ def gen_rnd_exp(fset, tset, max_depth):
 		arg1 = gen_rnd_exp(fset, tset, max_depth-1)
 		arg2 = gen_rnd_exp(fset, tset, max_depth-1)
 
-		exp = func + '(' + arg1 + ',' + arg2 + ')'
+		exp = [func, arg1, arg2]
 
 	return exp
 
@@ -49,5 +93,5 @@ def choose_rnd_element(set):
 
 
 population = init(n)
-print(population)
-print(fitness(population[0]))
+print(population[0])
+print("Value: " + str(eval(population[0], -1.0)))
