5a50f5d0dcfcb7d79c007c0ae008bc3779148949
Alexandros Georgousis <alexgeo2395@gmail.com>
2019-12-23 23:19:05 +0200

    Removed reproduction

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from matplotlib import pyplot as plt
 agent = Agent()
 agent.train()
 
-# Extract fitness scores
+# Plot average fitness scores
 gens = agent.gens
 scores = [[indiv[1] for indiv in pop] for pop in gens]
 avg_scores = [np.mean(pop) for pop in scores]
 
-# Plot fitness scores
-# plt.plot([i+1 for i in range(len(avg_scores))], avg_scores)
-# plt.show()
+print(avg_scores)
+plt.plot([i+1 for i in range(len(avg_scores))], avg_scores)
+plt.show()
 
-agent.run()
+# agent.run()
