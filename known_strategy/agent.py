534b1ea954025bce89bcfba9ffde4a1a78bfd24f
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-01-11 19:44:14 +0000

    Setup agent skeleton

---
+++ known_strategy/agent.py
@@ -0,0 +1,16 @@
+import gym
+
+# Types
+Program = [[str]]
+
+class Agent:
+	
+	def __init__(self):
+		self._T = ["pa", '0', 'L', 'R']
+		self._F = ["IFLTE"]
+
+	def _gen_init_pop(self) -> [[str]]:
+		pass
+
+	def _fit(p: Program) -> float:
+		pass
---
+++ known_strategy/test.py
@@ -0,0 +1,9 @@
+
+
+def test(a: str) -> int: 
+	if a == "correct":
+		return 0
+	else:
+		return 1
+
+print(test(1))
