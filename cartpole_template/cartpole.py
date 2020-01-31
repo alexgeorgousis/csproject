bfde1414ec7b22373a8087d62c142ac8f6845ea6
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-01-31 16:56:24 +0000

    Cartpole data object: included function arity in function set

--- cartpole_template/cartpole.py
+++ cartpole_template/cartpole.py
@@ -7,9 +7,9 @@ for the Cartpole-v0 environment.
 info = {
     "env_name": "CartPole-v0",
     "T": ["pa", "pv", "0.0", "0.025", "L", "R"],
-    "F": ["IFLTE"],
-    "program_depth": 2,
-    "pop_size": 100,
+    "F": {"IFLTE": {"arity": 4}},
+    "max_depth": 2,
+    "pop_size": 1,
     "num_eps": 100,
     "max_gens": 1,
     "term_fit": 195.0
