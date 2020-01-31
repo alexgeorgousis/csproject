358ae3d69640b9c28bc2069a5a5b2e3260adc213
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-01-31 16:57:14 +0000

    Implemented full and growth method for generating random programs

--- cartpole_template/gp_agent.py
+++ cartpole_template/gp_agent.py
@@ -1,4 +1,5 @@
 import gym
+import numpy as np
 
 class GPAgent:
     """
@@ -11,8 +12,8 @@ class GPAgent:
         self.env_name = info["env_name"]
         self.T = info["T"]
         self.F = info["F"]
-        self.program_depth = info["program_depth"]
-        self.pop_size = info["pop_size"]
+        self.max_d = info["max_depth"]
+        self.n = info["pop_size"]
         self.num_eps = info["num_eps"]
         self.max_gens = info["max_gens"]
         self.term_fit = info["term_fit"]
@@ -35,7 +36,43 @@ class GPAgent:
         env.close()
 
     def _train(self):
-        return ['IFLTE', '0', '0', '1', '0']
+        best_program = []
+
+        # Generate initial population
+        init_pop = [self._gen_prog(self.max_d, 'grow') for _ in range(self.n)]
+        for p in init_pop:
+            print(p)
+
+        # Evolution loop
+
+        return best_program
 
     def _eval(self, p, obs):
         return 0
+
+    def _gen_prog(self, max_d, method):
+        """
+        Generates a random program with a fixed max depth using the terminal and function sets. 
+        Supported methods: full and growth.
+
+        max_d: maximum program depth
+        method: "grow" | "full"
+        """
+        
+        prog = None
+
+        len_T = len(self.T)
+        len_F = len(self.F)
+        p_term = len_T / (len_T + len_F)  # probability of choosing a terminal
+
+        # Pick random terminal
+        if max_d == 0 or (method == "grow" and p_term > np.random.rand()):
+            prog = np.random.choice(self.T)
+        else:
+            # Pick random function and recursively generate random arguments for it
+            func = np.random.choice(list(self.F.keys()))
+            arity = self.F[func]["arity"]
+            args = [self._gen_prog(max_d-1, method) for _ in range(arity)]
+            prog = [func] + args
+
+        return prog
