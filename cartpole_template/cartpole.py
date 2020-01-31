4239160928050ca099353e9c393f35f452ed26fc
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-01-31 14:27:16 +0000

    Created cartpole data object

---
+++ cartpole_template/cartpole.py
@@ -0,0 +1,16 @@
+
+"""
+Data object representation of GP agent 
+for the Cartpole-v0 environment.
+"""
+
+info = {
+    "env_name": "CartPole-v0",
+    "T": ["pa", "pv", "0.0", "0.025", "L", "R"],
+    "F": ["IFLTE"],
+    "program_depth": 2,
+    "pop_size": 100,
+    "num_eps": 100,
+    "max_gens": 1,
+    "term_fit": 195.0
+}
