ce2a74015b60d9efcf3f159387ff581869664726
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-01-31 20:43:18 +0000

    Implemented basic type system for variables

--- gp_envs/cartpole.py
+++ gp_envs/cartpole.py
@@ -5,9 +5,17 @@ for the Cartpole-v0 environment.
 
 info = {
     "env_name": "CartPole-v0",
-    "T": ["pa", "pv", "0.0", "0.025", "L", "R"],
+    "T": {
+        "pa": {"type":"Number"}, 
+        "pv": {"type":"Number"}, 
+        "0.0": {"type":"Number"}, 
+        "0.025": {"type":"Number"}, 
+        "0": {"type":"Action"}, 
+        "1": {"type":"Action"}
+    },
     "F": {"IFLTE": {"arity": 4}},
     "max_depth": 2,
+    "term_growth_rate": 0.5,
     "pop_size": 1,
     "num_eps": 100,
     "max_gens": 1,
--- gp_envs/gp_agent.py
+++ gp_envs/gp_agent.py
@@ -10,9 +10,14 @@ class GPAgent:
 
     def __init__(self, info):
         self.env_name = info["env_name"]
+        
+        # Program structure params
         self.T = info["T"]
         self.F = info["F"]
         self.max_d = info["max_depth"]
+        self.growth_rate = info["term_growth_rate"]  # probability of growing a terminal instead of a function
+        
+        # GP experiment params
         self.n = info["pop_size"]
         self.num_eps = info["num_eps"]
         self.max_gens = info["max_gens"]
@@ -27,7 +32,7 @@ class GPAgent:
         done = False
 
         while not done:
-            env.render()
+            # env.render()
             action = self._eval(best_program, obs)
             obs, rew, done, _ = env.step(action)
             net_reward += rew
@@ -50,7 +55,7 @@ class GPAgent:
     def _eval(self, p, obs):
         return 0
 
-    def _gen_prog(self, max_d, method):
+    def _gen_prog(self, max_d, method, type="Action"):
         """
         Generates a random program with a fixed max depth using the terminal and function sets. 
         Supported methods: full and growth.
@@ -61,13 +66,11 @@ class GPAgent:
         
         prog = None
 
-        len_T = len(self.T)
-        len_F = len(self.F)
-        p_term = len_T / (len_T + len_F)  # probability of choosing a terminal
-
         # Pick random terminal
-        if max_d == 0 or (method == "grow" and p_term > np.random.rand()):
-            prog = np.random.choice(self.T)
+        if max_d == 0 or (method == "grow" and self.growth_rate > np.random.rand()):
+            # Only choose from the terminals that have the correct type
+            filt_terms = list(dict(filter(lambda term: term[1]["type"]==type, self.T.items())).keys())
+            prog = np.random.choice(filt_terms)
         else:
             # Pick random function and recursively generate random arguments for it
             func = np.random.choice(list(self.F.keys()))
