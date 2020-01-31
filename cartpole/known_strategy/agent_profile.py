ba8ddf6c69eed3a18580df7867f640fab8022bca
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-01-31 17:24:37 +0000

    Restructured subdirs

--- cartpole/known_strategy/agent_profile.py
+++
@@ -1,7 +0,0 @@
-from agent import Agent
-import numpy as np
-
-
-np.random.seed(0)
-my_agent = Agent()
-my_agent.train()
--- cartpole/known_strategy/main.py
+++
@@ -1,5 +0,0 @@
-from agent import Agent
-
-
-my_agent = Agent()
-my_agent.run()
