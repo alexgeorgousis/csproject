d82afe04fdf93111779538dbc56c99b5796e0efb
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-02-01 13:45:18 +0000

    Implemented selection and finished the full GP implementation

--- gp_envs/gp_agent.py
+++ gp_envs/gp_agent.py
@@ -5,7 +5,7 @@ class GPAgent:
     """
     GP template class. Implements all the functionality of a GP agent 
     for a gym environment and is parameterised by a data object with
-    the parameters for a specific environment. 
+    the parameters required to train an agent on a specific environment. 
     """
 
     def __init__(self, info):
@@ -25,75 +25,60 @@ class GPAgent:
 
     def run(self):
         best_program = self._train()
-        
-        env = gym.make(self.env_name)
-        obs = env.reset()
-        net_reward = 0
-        done = False
+        print(best_program)
 
-        while not done:
-            # env.render()
-            action = self._eval(best_program, obs)
-            obs, rew, done, _ = env.step(action)
-            net_reward += rew
+        env = gym.make(self.env_name)
+        net_reward = 0
+
+        # Run episodes
+        for _ in range(self.num_eps):
+            ep_reward = 0
+            done = False
+            obs = env.reset()
+
+            # Run single episode
+            while not done:
+                action = self._eval(best_program, obs)
+                obs, rew, done, _ = env.step(action)
+                ep_reward += rew
+                
+            net_reward += ep_reward
         
-        print("Net reward: {}".format(net_reward))
+        print("Reward: {}".format(net_reward / self.num_eps))
         env.close()
 
     def _train(self):
         best_program = []
 
         # Generate initial population
-        init_pop = [self._gen_prog(self.max_d, 'grow') for _ in range(self.n)]
+        init_pop = [self._gen_prog(self.max_d, 'full') for _ in range(self.n)]
 
         # Evolution loop
-        for _ in range(self.max_gens):
-            fit_scores = self._fit(init_pop)
-            print(fit_scores)
+        current_pop = init_pop
+        for gen_idx in range(self.max_gens):
+            print("Generation {}".format(gen_idx))
+            print(current_pop)
+            
+            # Evaluate population fitness
+            fit_scores = self._fit(current_pop)
+
+            # If fitness termination criterion is met, select best program and stop evolution
+            max_fitness = max(fit_scores)
+            if max_fitness >= self.term_fit:
+                best_program = current_pop[fit_scores.index(max_fitness)]
+                break
+
+            # If this is the last generation, pick the best individual
+            if gen_idx >= self.max_gens-1:
+                best_program_idx = fit_scores.index(max(fit_scores))
+                best_program = current_pop[best_program_idx]
+
+            # Selection & reproduction
+            else:
+                next_pop = [self._select(current_pop, fit_scores) for _ in range(self.n)]
+                current_pop = next_pop
 
         return best_program
-
-    def _fit(self, pop):
-        scores = []
-
-        env = gym.make(self.env_name)
-        for p in pop:
-            net_reward = 0
-
-            # Run episodes
-            for _ in range(self.num_eps):
-                ep_reward = 0
-                done = False
-                obs = env.reset()
-
-                # Run single episode
-                while not done:
-                    action = self._eval(p, obs)
-                    obs, rew, done, _ = env.step(action)
-                    ep_reward += rew
-                    
-                net_reward += ep_reward
-
-            # Store average reward
-            scores.append(net_reward / self.num_eps)
-
-        return scores
-
-    def _eval(self, p, obs):
-        action = 0
-
-        # Terminals
-        if type(p) is not list:
-            term = self.T[p]
-            if term["token"] == "StateVar":  # variable
-                return obs[term["state_index"]]
-            elif term["token"] == "Constant":  # constant
-                if term["type"] == "Float":  # float
-                    return float(p)
-
-        # Functions
-
-        return action
 
     def _gen_prog(self, max_d, method, type="Action"):
         """
@@ -124,3 +109,82 @@ class GPAgent:
                 prog = np.random.choice(filt_terms)
 
         return prog
+
+    def _fit(self, pop):
+        scores = []
+
+        env = gym.make(self.env_name)
+        for p in pop:
+            net_reward = 0
+
+            # Run episodes
+            for _ in range(self.num_eps):
+                ep_reward = 0
+                done = False
+                obs = env.reset()
+
+                # Run single episode
+                while not done:
+                    action = self._eval(p, obs)
+                    obs, rew, done, _ = env.step(action)
+                    ep_reward += rew
+                    
+                net_reward += ep_reward
+
+            # Store average reward
+            scores.append(net_reward / self.num_eps)
+        
+        env.close()
+        return scores
+
+    def _select(self, pop, fit_scores):
+        """
+        Fitness Proportionate Selection (Roulette Wheel Selection)
+        """
+
+        selected = None
+
+        F = sum(fit_scores)
+        r = np.random.uniform(0, F)
+
+        acc = 0
+        for i in range(len(fit_scores)):
+            acc += fit_scores[i]
+            if acc > r:
+                selected = pop[i]
+                break
+
+        return selected
+
+    def _eval(self, p, obs):
+        action = 0
+
+        # Terminals
+        if type(p) is not list:
+            term = self.T[p]
+            if term["token"] == "StateVar":  # state variable
+                return obs[term["state_index"]]
+            elif term["token"] == "Constant":  # constant
+                if term["type"] == "Float":  # float
+                    return float(p)
+            elif term["type"] == "Action":  # action
+                return int(p)
+
+        # Functions
+        else:
+            fname = p[0]
+            args = [self._eval(p[i+1], obs) for i in range(self.F[fname]["arity"])]
+            if fname == "IFLTE":
+                action = self._IFLTE(args)
+
+        return action
+
+    def _IFLTE(self, args):
+        action = 0
+
+        if args[0] <= args[1]:
+            action = args[2]
+        else:
+            action = args[3]
+
+        return action
