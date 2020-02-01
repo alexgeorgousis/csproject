afa30155e5a3b6405d6f3041935638f1a835c93e
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-02-01 17:42:46 +0000

    Set IFLTE return type to float for Pendulum

--- gp_envs/pendulum.py
+++ gp_envs/pendulum.py
@@ -5,7 +5,7 @@ for the Pendulum-v0 environment.
 
 info = {
     "env_name": "Pendulum-v0",
-    "program_type": "Float"
+    "program_type": "Float",
     "T": {
         "costheta": {"type":"Float", "token": "StateVar", "state_index": 0},
         "sintheta": {"type":"Float", "token": "StateVar", "state_index": 1},
@@ -16,7 +16,7 @@ info = {
         "1.0":  {"type":"Float", "token": "Constant"},
         "2.0":  {"type":"Float", "token": "Constant"},
     },
-    "F": {"IFLTE": {"arity": 4, "type": "Action", "arg_types": ["Float", "Float", "Action", "Action"]}},
+    "F": {"IFLTE": {"arity": 4, "type": "Float", "arg_types": ["Float", "Float", "Float", "Float"]}},
     "max_depth": 2,
     "term_growth_rate": 1.0,
     "pop_size": 1,
