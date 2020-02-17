0b88702d9e4c2d645093717ecb3e75aeabce72d7
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-02-17 08:37:12 +0000

    Removed T and F sets

--- gp_gym/pendulum_info.py
+++ gp_gym/pendulum_info.py
@@ -5,16 +5,6 @@ for the Pendulum-v0 environment.
 
 info = {
     "env_name": "Pendulum-v0",
-    "program_type": "Float",
-    "T": {
-        "costheta": {"type":"Float", "token": "ObsVar", "obs_index": 0},
-        "sintheta": {"type":"Float", "token": "ObsVar", "obs_index": 1},
-        "thetadot": {"type":"Float", "token": "ObsVar", "obs_index": 2},
-        "0.0": {"type":"Float", "token": "Constant"},
-        "1.0": {"type":"Float", "token": "Constant"},
-        "-1.0": {"type":"Float", "token": "Constant"},
-    },
-    "F": {"IFLTE": {"arity": 4, "type": "Float", "arg_types": ["Float", "Float", "Float", "Float"]}},
     "max_depth": 2,
     "term_growth_rate": 1.0,
     "method": 'grow',
