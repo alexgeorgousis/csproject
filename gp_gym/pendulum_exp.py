e3d0cf225c0e0c54b033d49488c9514f269f97c8
Alexandros Georgousis <alexgeo2395@gmail.com>
2020-02-22 12:53:25 +0000

    Print statistics in pendulum exp

--- gp_gym/pendulum_exp.py
+++ gp_gym/pendulum_exp.py
@@ -8,11 +8,11 @@ random.seed(0)
 
 """Experiment parameters"""
 info["pop_size"] = 100
-info["max_gens"] = 10
+info["max_gens"] = 20
 info["max_depth"] = 2
 info["mutation_rate"] = 0.1
 info["num_eps"]  = 5 # for training
-info["num_time_steps"] = 200
+info["num_time_steps"] = 400
 info["term_fit"] = -500
 
 
@@ -22,6 +22,7 @@ info["term_fit"] = -500
 agent = Pendulum(info)
 best_program = agent.train()
 print("\nBest program:\n{}".format(best_program))
+print("\n{}".format(agent.logbook))
 
 # Evaluate solution
 num_eps = 100
