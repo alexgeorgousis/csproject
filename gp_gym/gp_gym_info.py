ff36247b8801db64d3870523d056589efa3debba
Alex Georgousis <alexgeo2395@gmail.com>
2020-03-16 08:41:53 +0000

    Added blank environment name to info data

--- gp_gym/gp_gym_info.py
+++ gp_gym/gp_gym_info.py
@@ -4,13 +4,14 @@ for a Gym environment.
 """
 
 info = {
+    "env_name": "",
     "max_depth": 2,
     "method": 'grow',
     "pop_size": 1,
     "num_time_steps": 200,
     "num_eps": 100,
     "max_gens": 1,
-    "term_fit": -800.0,
+    "term_fit": 0.0,
     "mutation_rate": 0.1,
     "tournament_size": 1
 }
